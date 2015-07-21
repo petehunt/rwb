8941f8ef6d080b0c361cb839ef90f79ff3bcac1a
Pete Hunt <pete@smyte.com>
2015-07-21 17:32:15 -0500

    It works omg

--- Workflows.js
+++ Workflows.js
@@ -124,12 +124,13 @@ var Workflows = {
       };
 
       new WebpackDevServer(webpack(config), {
+        contentBase: dirPath,
         publicPath: config.output.publicPath,
         hot: true,
         historyApiFallback: true
       }).listen(3000, 'localhost', function (err, result) {
         errGuard(err);
-        console.log('Listening at localhost:3000');
+        console.log('Serving ' + dirPath + ' at localhost:3000');
       });
     });
   },
