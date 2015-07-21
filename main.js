df662488be3e58d8d31ca744bced6aae447964b9
Pete Hunt <pete@smyte.com>
2015-07-21 16:21:16 -0500

    Add main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+'use strict';
+
+var Workflows = require('./Workflows');
+
+var argv = require('yargs').argv;
+var invariant = require('invariant');
+
+var command = argv._[0];
+invariant(command, 'A command was not provided');
+invariant(
+  Workflows.hasOwnProperty(command),
+  'Invalid command: %s (acceptable commands: %s)',
+  command,
+  Object.keys(Workflows).join(', ')
+);
+
+Workflows[command](function() {});
